5c8449c54f25e7580c3691fde4c53883ec644713
Harivatsa <gaharivatsa@gmail.com>
2023-09-30 22:30:20 +0530

    'fc2'

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,13 +7,13 @@ export const CardsComponent = (props) => {
     <Card variant="outlined" sx = {{background: 'rgb(221,74,226)', 
     backgroundImage: 'linear-gradient(90deg, rgba(221,74,226,1) 14%, rgba(32,99,196,1) 96%)'}}>
           <CardContent>
-            <Typography variant="h5" component="div">
+            <Typography variant="h5" component="div" sx ={{color:'white'}}>
               {props.title}
             </Typography>
-            <Typography sx={{ mb: 1 }} variant = 'h6' color="text.secondary">
+            <Typography sx={{ mb: 1,color:'white' }} variant = 'h6' color="text.secondary">
               {props.subtitle}
             </Typography>
-            <Typography variant="body1">
+            <Typography variant="body1" sx ={{color:'white'}}>
               {props.content}
             </Typography>
           </CardContent>
